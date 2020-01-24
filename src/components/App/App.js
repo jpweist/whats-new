d01b270f57420dbb1b50d6a81d92c3ce71383115
Jim Weist <jp_weist@yahoo.com>
2020-01-23 18:42:08 -0700

    Just settin up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+// import Menu from '../../components/Menu';
+// import '../../components/Menu.css';
 
 class App extends Component {
   constructor() {
@@ -13,7 +15,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        jim jim jim
       </div>
     );
   }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,5 +2,22 @@ import React from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
+class Menu extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      local
+    }
+  }
+
+  render () {
+    return (
+      <div className="menu">
+        menu
+      </div>
+    );
+  }
+  }
 
 export default Menu;
